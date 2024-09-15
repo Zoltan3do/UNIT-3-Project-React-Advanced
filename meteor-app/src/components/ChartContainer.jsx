ddaca8c8a93cb598d56939a23ca286b753b247cf
Zoltan3do <baronemanuel05@gmail.com>
2024-09-15 19:18:28 +0200

    handleData chart container

--- meteor-app/src/components/ChartContainer.jsx
+++ meteor-app/src/components/ChartContainer.jsx
@@ -1,17 +1,34 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 import RainChart from "./RainChart";
 
 
 function ChartContainer({ dati }) {
-    const data = [
-        { name: 'Piovoso', value: dati.list[0].wind.speed * 10 },
-        { name: 'Soleggiato', value: dati.list[4].wind.speed * 10 },
-        { name: 'Estremo', value: dati.list[7].wind.speed * 10 },
-    ];
+
+    const [data, setData] = useState(null)
+
+    const handleData = () => {
+        if (dati.list) {
+            setData([
+                { name: 'Piovoso', value: dati.list[0].wind.speed * 10 },
+                { name: 'Soleggiato', value: dati.list[4].wind.speed * 10 },
+                { name: 'Estremo', value: dati.list[7].wind.speed * 10 },
+            ]);
+        }
+    }
+
+    useEffect(() => {
+        handleData()
+        console.log(data)
+    }, [dati])
+
     return (
         <div className="mt-5">
             <h4 className="text-light">Possibilità di pioggia</h4>
-            <RainChart data={data} />
+            {
+                dati.list && <RainChart data={data} />
+            }
+
         </div>
     );
 }
